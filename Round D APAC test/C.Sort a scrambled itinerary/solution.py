e3bc4553a22d36280aaea7f19b9807f41811f431
Diljit PR <diljitpr@gmail.com>
2014-11-11 10:51:51 +0530

    Lines of Code reduced..

--- Round D APAC test/C.Sort a scrambled itinerary/solution.py
+++ Round D APAC test/C.Sort a scrambled itinerary/solution.py
@@ -3,18 +3,11 @@ for y in range(1,testcases+1):
 	number_of_stations = int(raw_input());
 	stations ={};
 	for x in range(number_of_stations*2):
-		if (x%2==0):
-			src = raw_input();
-		else:
-			dest = raw_input();
-			stations[src]=dest
+		stations[raw_input()] = raw_input()
 	for x in stations.keys():
 		if x not in stations.values():
 			source = x ;
 			break;
-	for x in stations.values():
-		if x not in stations.keys():
-			destination = x
 	print "Case #%s:"%(y),
 	while stations.has_key(source):
 		print source+"-"+stations[source],
